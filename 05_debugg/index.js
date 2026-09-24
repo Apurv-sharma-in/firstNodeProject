3a24f0425226f7de3c7ca16c02a81df331274178
Apurv Sharma <apurvsharma.in@gmail.com>
2026-09-24 19:24:17 +0530

    Express server is running

---
+++ 05_debugg/index.js
@@ -0,0 +1,14 @@
+function addOne(a,b){
+    let sum = a+b+1;
+    return sum
+}
+function addTwo(a,b){
+    let sum = a+b+2;
+    return sum
+}
+function add(a,b){
+    let sum =addOne(a,b)+addTwo(a,b);
+    return sum
+}
+let result = add(1,1);
+console.log("result :",result);
---
+++ 06_Start_with_Express/app.js
@@ -0,0 +1,29 @@
+//start with express use {npm install express --save}
+const http = require("http");
+const express = require('express');
+
+const app = express();
+app.use((req,res,next)=>{
+    console.log("Output form middleware 1 ");
+    next();
+});
+app.use((req,res,next)=>{
+    console.log("Output form middleware 2 ");
+    res.send(`
+        <h1>Hii this is Express</h1>
+        `)
+});
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Server is up and running on port 3000! Ready to handle requests.")
+})
+
+// const server = http.createServer(app);
+//user these two line the express will give a new fucntion
+
+// server.listen(3000,()=>{
+//     console.log("Server is running ");
+// })

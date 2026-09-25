003a2dee0ba5e3de3b2d00731bca3288e8264a6f
Apurv Sharma <apurvsharma.in@gmail.com>
2026-09-25 20:22:37 +0530

    How to use Dynamic-routes

---
+++ 10_Dynamic-routes/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/welcome/:username',(req,res)=>{
+    const username = req.params.username;
+    const role = req.query.role;
+    res.send(`Welcome ${username} your role is  ${role}`);
+    
+})
+
+
+
+
+
+app.listen(port,()=>{
+    console.log('Sever is running');
+});
